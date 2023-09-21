e44f51c1eff671e6cfc43649a82eee0ce4725e3e
Pablo Sanchez <psanchezdev345@gmail.com>
2023-09-20 19:35:36 -0600

    added header and footer files base

---
+++ footer.php
@@ -0,0 +1,12 @@
+<?php wp_footer(); ?>
+
+<!-- Footer -->
+<footer>
+  <div class="footer-main">
+    <div class="footer-menu">
+      <nav>
+        footer menu
+      </nav>
+    </div>
+  </div>
+</footer>
---
+++ header.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?> >
+
+<!-- Header Start -->
+<header id="header" class="header">
+
+  <div class="header-main">
+      <!-- Logo or site title -->
+    <div class="branding">
+      <?php
+        if( has_custom_logo() ) {
+          the_custom_logo();
+        } else {
+          ?>
+            <a href="<?php esc_url( bloginfo( 'url' ) ); ?>">
+              <div class="site-title"><?php bloginfo( 'title' ); ?></div>
+            </a>
+          <?php
+        }
+      ?>
+    </div>
+
+    <!-- Mobile menu toggle -->
+    <button id="mobile-menu-toggle" class="mobile-menu-toggle"><i class="fa fa-solid fa-bars fa-2x"></i></button>
+
+    <!-- Nav Menu -->
+    <nav id="main-menu" class="main-menu">
+      Nav here
+    </nav>
+  </div>
+
+</header>
