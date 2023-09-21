efd4e8100c26d7527e9fb6380ede48a02ccb1a81
Pablo Sanchez <psanchezdev345@gmail.com>
2023-09-20 20:37:39 -0600

    added main stylesheet

--- functions.php
+++ functions.php
@@ -3,7 +3,8 @@
 function cloud_peak_enqueue_scripts() {
   //load theme info stysheet
   wp_enqueue_style( 'cloud-peak-theme-info', get_stylesheet_uri(), array(), wp_get_theme()->get( 'version' ), 'all' );
- 
+  //load main style sheet
+  wp_enqueue_style( 'cloud-peak-main-styles', get_template_directory_uri() . '/assets/css/styles-main.css', array(), wp_get_theme()->get( 'version' ), 'all' );
 }
 
 add_action( 'wp_enqueue_scripts', 'cloud_peak_enqueue_scripts' );
