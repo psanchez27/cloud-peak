1f4fec055fa0b3c2dfc2045b4a804d726517a85c
Pablo Sanchez <psanchezdev345@gmail.com>
2023-10-11 20:40:59 -0600

    started adding container and did come clean up

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
 <!-- Footer -->
 <footer>
   <div class="footer-main">
-  <p class="copyright"><?php echo get_theme_mod( 'set_copyright', 'Copyright Cloud Peak Systems - All Rights Reserved' ); ?></p>
+    <p class="copyright"><?php echo get_theme_mod( 'set_copyright', 'Copyright Cloud Peak Systems - All Rights Reserved' ); ?></p>
     <div class="footer-menu">
       <nav>
         <?php
